1fb8e84625739f2711d5f677cd3c01ff05533a36
Adnan Latif Gazi <adnangazi.ag@gmail.com>
2022-06-30 12:20:14 +0200

    AlphaNotary is complete!

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,13 @@
+/**
+* local function of all GUI content
+*/
 (function () {
+  /**
+  * query selector
+  */
   const select = (el, all = false) => {
     el = el.trim();
+
     if (all) {
       return [...document.querySelectorAll(el)];
     } else {
@@ -8,6 +15,9 @@
     }
   }
 
+  /**
+  * add event listener event load
+  */
   const on = (type, el, listener, all = false) => {
     if (all) {
       select(el, all).forEach(e => e.addEventListener(type, listener));
@@ -16,18 +26,28 @@
     }
   }
 
+  /**
+  * on scroll event load
+  */
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener);
   }
 
   let navbarlinks = select('#navbar .scrollto', true);
 
+  /**
+  * navbar behaviour
+  */
   const navbarlinksActive = () => {
     let position = window.scrollY + 200;
+
     navbarlinks.forEach(navbarlink => {
       if (!navbarlink.hash) return;
+
       let section = select(navbarlink.hash);
+
       if (!section) return;
+
       if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
         navbarlink.classList.add('active');
       } else {
@@ -36,16 +56,28 @@
     })
   }
 
+  /**
+  * on load event navbar load
+  */
   window.addEventListener('load', navbarlinksActive);
   onscroll(document, navbarlinksActive);
 
+  /**
+  * scroll behaviour
+  */
   const scrollto = (el) => {
     let header = select('#header');
     let offset = header.offsetHeight;
+
     if (!header.classList.contains('header-scrolled')) {
       offset -= 10;
     }
+
     let elementPos = select(el).offsetTop;
+
+    /**
+    * scroll behaviour
+    */
     window.scrollTo({
       top: elementPos - offset,
       behavior: 'smooth'
@@ -55,6 +87,9 @@
   let selectHeader = select('#header');
 
   if (selectHeader) {
+    /**
+    * header scroll behaviour
+    */
     const headerScrolled = () => {
       if (window.scrollY > 100) {
         selectHeader.classList.add('header-scrolled');
@@ -62,6 +97,10 @@
         selectHeader.classList.remove('header-scrolled');
       }
     }
+    
+    /**
+    * on load event header scroll down load
+    */
     window.addEventListener('load', headerScrolled);
     onscroll(document, headerScrolled);
   }
@@ -69,6 +108,9 @@
   let backtotop = select('.back-to-top');
   
   if (backtotop) {
+    /**
+    * toggle back to top behaviour
+    */
     const toggleBacktotop = () => {
       if (window.scrollY > 100) {
         backtotop.classList.add('active');
@@ -76,16 +118,26 @@
         backtotop.classList.remove('active');
       }
     }
+
+    /**
+    * on load event toggle back to top load
+    */
     window.addEventListener('load', toggleBacktotop);
     onscroll(document, toggleBacktotop);
   }
 
+  /**
+  * on click event mobile navbar toggle load
+  */
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile');
     this.classList.toggle('bi-list');
     this.classList.toggle('bi-x');
   })
 
+  /**
+  * on click event navbar dropdown load
+  */
   on('click', '.navbar .dropdown > a', function(e) {
     if (select('#navbar').classList.contains('navbar-mobile')) {
       e.preventDefault();
@@ -93,20 +145,28 @@
     }
   }, true);
 
+  /**
+  * on click event navbar load
+  */
   on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
       e.preventDefault();
       let navbar = select('#navbar');
+
       if (navbar.classList.contains('navbar-mobile')) {
         navbar.classList.remove('navbar-mobile');
         let navbarToggle = select('.mobile-nav-toggle');
         navbarToggle.classList.toggle('bi-list');
         navbarToggle.classList.toggle('bi-x');
       }
+
       scrollto(this.hash);
     }
   }, true)
 
+  /**
+  * on load event load
+  */
   window.addEventListener('load', () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
@@ -115,6 +175,9 @@
     }
   });
 
+  /**
+  * clients slider behavior
+  */
   new Swiper('.clients-slider', {
     speed: 400,
     loop: true,
@@ -148,6 +211,9 @@
     }
   });
 
+  /**
+  * testimonials slider behavior
+  */
   new Swiper('.testimonials-slider', {
     speed: 600,
     loop: true,
@@ -172,7 +238,13 @@
     }
   });
 
+  /**
+  * aos initialize
+  */
   function aos_init() {
+    /**
+    * aos initialize
+    */
     AOS.init({
       duration: 1000,
       easing: "ease-in-out",
@@ -181,19 +253,22 @@
     });
   }
 
+  /**
+  * on load event aos load
+  */
   window.addEventListener('load', () => {
     aos_init();
   });
 })();
 
 /**
-* launch the controller when the application is open
+* launch the dark mode animations when the application is open
 */
 Animations = {
   darkMode: [document.getElementById("darkMode"), true],
 
   /**
-  * launch the controller when the application is open
+  * launch the dark mode animations when the application is open
   */
   darkModeSwitcher: async () => {
     if (Animations.darkMode[1]) {
@@ -203,6 +278,7 @@ Animations = {
       document.documentElement.setAttribute("data-theme", "light");
       Animations.darkMode[0].title = 'Dark mode off';
     }
+    
     Animations.darkMode[1] = !Animations.darkMode[1];
   }
 }
